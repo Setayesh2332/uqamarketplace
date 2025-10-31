f1cce351e4a9f7fe10b2912e4df422ffc3e981e5
Rhazouanihouda <rhazouanih98@gmail.com>
2025-10-31 10:57:00 -0400

    Initial commit: Login page

---
+++ uqammarketplace/src/pages/login.js
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+
+function App() {
+  return (
+    <MDBContainer fluid>
+      <MDBRow className="d-flex justify-content-center align-items-center h-100">
+        <MDBCol col="12">
+          <MDBCard
+            className="text-dark my-5 mx-auto"
+            style={{
+              borderRadius: "1rem",
+              maxWidth: "400px",
+              backgroundColor: "#ADD8E6",
+            }}
+          >
+            <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+              <p className="text-dark mb-5">
+                Please enter your login and password!
+              </p>
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-dark"
+                label="Email address"
+                id="formControlLg"
+                type="email"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-dark"
+                label="Password"
+                id="formControlLg"
+                type="password"
+                size="lg"
+              />
+              <p className="small mb-3 pb-lg-2">
+                <a className="text-dark" href="#!">
+                  Forgot password?
+                </a>
+              </p>
+              <MDBBtn outline className="mx-2 px-5" color="dark" size="lg">
+                Login
+              </MDBBtn>
+              <div className="d-flex flex-row mt-3 mb-5">
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "#333" }}
+                >
+                  <MDBIcon fab icon="facebook-f" size="lg" />
+                </MDBBtn>
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "#333" }}
+                >
+                  <MDBIcon fab icon="twitter" size="lg" />
+                </MDBBtn>
+                <MDBBtn
+                  tag="a"
+                  color="none"
+                  className="m-3"
+                  style={{ color: "#333" }}
+                >
+                  <MDBIcon fab icon="google" size="lg" />
+                </MDBBtn>
+              </div>
+              <div>
+                <p className="mb-0">
+                  Don't have an account?{" "}
+                  <a href="#!" className="text-dark fw-bold">
+                    Sign Up
+                  </a>
+                </p>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
+
+export default App;
