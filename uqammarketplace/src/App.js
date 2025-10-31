ca1a67bb95868263e53b52f6ea7b277502c6f6e9
Setayesh <setayesh2332@gmail.com>
2025-10-31 15:37:56 -0400

    Mise a jour pour App.js pour le form du page du vente d'un article

--- uqammarketplace/src/App.js
+++ uqammarketplace/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import SignUp from "./pages/signUp";
 import Profile from "./pages/profile";
+import Sell from "./pages/Sell";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/sell" element={<Sell />} />
       </Routes>
     </Router>
   );
